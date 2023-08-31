65f5fc0bf68bb204dd3a72ce287be8860e995a0d
tinycodecraft <raymond.cw.yau@gmail.com>
2023-08-31 09:28:35 +0800

    align the button

--- GhostUI/ClientApp/src/containers/Login/LoginForm.tsx
+++ GhostUI/ClientApp/src/containers/Login/LoginForm.tsx
@@ -101,7 +101,6 @@ const LoginForm: FunctionComponent = () => {
                   variant='outlined'
                   label='User Name'
                   type='text'
-                  
                   value={userName}
                   error={isInvalid}
                   onChange={(e) => setUserName && setUserName(e.target.value)}
@@ -110,7 +109,6 @@ const LoginForm: FunctionComponent = () => {
                   variant='outlined'
                   label='Password'
                   type={show ? 'password' : 'text'}
-                  
                   value={password}
                   error={isInvalid}
                   onChange={(e) => setPassword && setPassword(e.target.value)}
@@ -121,7 +119,6 @@ const LoginForm: FunctionComponent = () => {
                     variant='outlined'
                     label='New Password'
                     type={show ? 'password' : 'text'}
-                    
                     value={newPassword}
                     error={isInvalid}
                     onChange={(e) => setNewPassword && setNewPassword(e.target.value)}
@@ -130,35 +127,22 @@ const LoginForm: FunctionComponent = () => {
                 ) : (
                   <></>
                 )}
-                <Checkbox
-                  label='Remember Me'
-                  checked={rememberMe}
-                  
-                  ripple={false}
-                  onChange={() => setRememberMe && setRememberMe(!rememberMe)}
-                />
-              <Typography color='gray' className='mt-4 text-center font-normal self-center'>
-                
-                <Button type='submit' color='green' className='flex items-center gap-3' size='lg'>
-                Login
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  fill='currentColor'
-                  className='w-6 h-6'
-                 
-                >
-                  <path
-                    fillRule='evenodd'
-                    d='M12 3.75a6.715 6.715 0 00-3.722 1.118.75.75 0 11-.828-1.25 8.25 8.25 0 0112.8 6.883c0 3.014-.574 5.897-1.62 8.543a.75.75 0 01-1.395-.551A21.69 21.69 0 0018.75 10.5 6.75 6.75 0 0012 3.75zM6.157 5.739a.75.75 0 01.21 1.04A6.715 6.715 0 005.25 10.5c0 1.613-.463 3.12-1.265 4.393a.75.75 0 01-1.27-.8A6.715 6.715 0 003.75 10.5c0-1.68.503-3.246 1.367-4.55a.75.75 0 011.04-.211zM12 7.5a3 3 0 00-3 3c0 3.1-1.176 5.927-3.105 8.056a.75.75 0 11-1.112-1.008A10.459 10.459 0 007.5 10.5a4.5 4.5 0 119 0c0 .547-.022 1.09-.067 1.626a.75.75 0 01-1.495-.123c.041-.495.062-.996.062-1.503a3 3 0 00-3-3zm0 2.25a.75.75 0 01.75.75A15.69 15.69 0 018.97 20.738a.75.75 0 01-1.14-.975A14.19 14.19 0 0011.25 10.5a.75.75 0 01.75-.75zm3.239 5.183a.75.75 0 01.515.927 19.415 19.415 0 01-2.585 5.544.75.75 0 11-1.243-.84 17.912 17.912 0 002.386-5.116.75.75 0 01.927-.515z'
-                    clipRule='evenodd'
-                  />
-                </svg>      
-              </Button>
-        
-              </Typography>                
+                <Checkbox label='Remember Me' checked={rememberMe} ripple={false} onChange={() => setRememberMe && setRememberMe(!rememberMe)} />
+                <Typography color='gray' className='mt-4 text-center font-normal self-center w-full'>
+                  <Button type='submit' color='green' className='items-center gap-3 w-full inline-block' size='lg'>
+                    <span>
+                      Login
+                      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' className='pl-2 inline w-8 h-6'>
+                        <path
+                          fillRule='evenodd'
+                          d='M12 3.75a6.715 6.715 0 00-3.722 1.118.75.75 0 11-.828-1.25 8.25 8.25 0 0112.8 6.883c0 3.014-.574 5.897-1.62 8.543a.75.75 0 01-1.395-.551A21.69 21.69 0 0018.75 10.5 6.75 6.75 0 0012 3.75zM6.157 5.739a.75.75 0 01.21 1.04A6.715 6.715 0 005.25 10.5c0 1.613-.463 3.12-1.265 4.393a.75.75 0 01-1.27-.8A6.715 6.715 0 003.75 10.5c0-1.68.503-3.246 1.367-4.55a.75.75 0 011.04-.211zM12 7.5a3 3 0 00-3 3c0 3.1-1.176 5.927-3.105 8.056a.75.75 0 11-1.112-1.008A10.459 10.459 0 007.5 10.5a4.5 4.5 0 119 0c0 .547-.022 1.09-.067 1.626a.75.75 0 01-1.495-.123c.041-.495.062-.996.062-1.503a3 3 0 00-3-3zm0 2.25a.75.75 0 01.75.75A15.69 15.69 0 018.97 20.738a.75.75 0 01-1.14-.975A14.19 14.19 0 0011.25 10.5a.75.75 0 01.75-.75zm3.239 5.183a.75.75 0 01.515.927 19.415 19.415 0 01-2.585 5.544.75.75 0 11-1.243-.84 17.912 17.912 0 002.386-5.116.75.75 0 01.927-.515z'
+                          clipRule='evenodd'
+                        />
+                      </svg>
+                    </span>
+                  </Button>
+                </Typography>
               </div>
-
 
               <Authenticator error={error} authStatus={status} handleOnFail={onFailedAuth} handleOnSuccess={onSuccessfulAuth} />
             </form>
